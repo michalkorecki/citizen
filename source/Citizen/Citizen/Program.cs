3805cfc05f77fa096c9f1af25d8099d7ad9c2f1a
Michał <korecki.m@gmail.com>
2017-03-01 15:53:09 +0100

    Cleanup async code, extract build details.

--- source/Citizen/Citizen/Program.cs
+++ source/Citizen/Citizen/Program.cs
@@ -20,64 +20,55 @@ namespace Citizen
             var client = new HttpClient();
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authenticationHeader);
 
-            var projectsDocument = FetchAsync(teamCityHost, "projects", client);
-            var projects = await GetProjectsAsync(projectsDocument);
+            var projectsDocument = await FetchAsync(teamCityHost, "httpAuth/app/rest/projects", client);
+            var projects = GetProjectsAsync(projectsDocument);
 
             foreach (var project in projects)
             {
-                var buildsDocument = FetchAsync(teamCityHost, $"projects/{project.Item1}", client);
-                var builds = await GetBuildsAsync(buildsDocument);
-                foreach (var build in builds)
+                var projectDetailsDocument = await FetchAsync(teamCityHost, $"httpAuth/app/rest/projects/{project.Item1}", client);
+                var buildTypes = GetBuildTypes(projectDetailsDocument);
+                foreach (var buildType in buildTypes)
                 {
-                    var buildTypeDocument = FetchAsync(teamCityHost, $"buildTypes/{build.Item1}", client);
-                    var buildsUrl = await GetBuildUrl(buildTypeDocument);
-                    var url = buildsUrl.Replace("/httpAuth/app/rest/", "");
-                    var b = await FetchAsync(teamCityHost, url, client);
-                    var count = b.Root.Attribute("count").Value;
-                    Console.WriteLine(count);
-                    Console.WriteLine(b);
+                    var buildHistoryDocument = await FetchAsync(teamCityHost, $"httpAuth/app/rest/buildTypes/{buildType.Item1}/builds", client);
+                    var builds = GetBuilds(buildHistoryDocument);
+
+                    foreach (var build in builds)
+                    {
+                        var buildDetails = await FetchAsync(teamCityHost, $"httpAuth/app/rest/builds/{build}", client);
+                        var buildInfo = $"{buildDetails.Root.Attribute("id").Value}:{buildDetails.Root.Attribute("status").Value}";
+                        Console.WriteLine(buildInfo);
+                    }
                 }
             }
         }
 
 
-        private static async Task<XDocument> FetchAsync(string host, string urlPath, HttpClient client)
+        private static async Task<XDocument> FetchAsync(string host, string resource, HttpClient client)
         {
             var root = new Uri(host);
-            var path = "httpAuth/app/rest/" + urlPath;
-            var url = new Uri(root, path);
+            var url = new Uri(root, resource);
+
+            Console.WriteLine($"Fetching {url}");
 
             var responseStream = await client.GetStreamAsync(url);
 
             return XDocument.Load(responseStream);
         }
 
-        private static async Task<IEnumerable<Tuple<string, string>>> GetProjectsAsync(Task<XDocument> projects)
-        {
-            var document = await projects;
+        private static IEnumerable<Tuple<string, string>> GetProjectsAsync(XDocument document) => document
+            .Root
+            .Elements("project")
+            .Select(p => Tuple.Create(p.Attribute("id").Value, p.Attribute("name").Value));
 
-            return document.Root.Elements("project").Select(p => Tuple.Create(p.Attribute("id").Value, p.Attribute("name").Value));
-        }
+        private static IEnumerable<Tuple<string, string>> GetBuildTypes(XDocument document) => document
+            .Root
+            .Element("buildTypes")
+            .Elements("buildType")
+            .Select(buildType => Tuple.Create(buildType.Attribute("id").Value, buildType.Attribute("name").Value));
 
-        private static async Task<IEnumerable<Tuple<string, string>>> GetBuildsAsync(Task<XDocument> builds)
-        {
-            var document = await builds;
-
-            return document.Root
-                .Element("buildTypes")
-                .Elements("buildType")
-                .Select(buildType => Tuple.Create(buildType.Attribute("id").Value, buildType.Attribute("name").Value));
-        }
-
-        private static async Task<string> GetBuildUrl(Task<XDocument> buildType)
-        {
-            var document = await buildType;
-
-            return document
-                .Root
-                .Element("builds")
-                .Attribute("href")
-                .Value;
-        }
+        private static IEnumerable<string> GetBuilds(XDocument document) => document
+            .Root
+            .Elements("build")
+            .Select(b => b.Attribute("id").Value);
     }
 }
