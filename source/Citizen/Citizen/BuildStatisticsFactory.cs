78c1712cec4aae2ada64524631d732a829e47bc8
Michał <korecki.m@gmail.com>
2017-03-02 20:06:03 +0100

    Build fetching refactored; simple statistics generation.

---
+++ source/Citizen/Citizen/BuildStatisticsFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Citizen
+{
+    public class BuildStatisticsFactory
+    {
+        public IEnumerable<Tuple<string, int, double, double>> CreateStatistics(IEnumerable<Build> builds, string status)
+        {
+            var buildsByBuildType = builds.Where(b => b.Status == status).GroupBy(b => b.BuildTypeName);
+            foreach (var group in buildsByBuildType)
+            {
+                var averageRunTime = group.Select(b => b.Finished - b.Started).Average(diff => diff.TotalSeconds);
+                var averageLagTime = group.Select(b => b.Started - b.Queued).Average(diff => diff.TotalSeconds);
+                yield return Tuple.Create(group.Key, group.Count(), averageLagTime, averageRunTime);
+            }
+        }
+    }
+}
--- source/Citizen/Citizen/Program.cs
+++ source/Citizen/Citizen/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -7,20 +9,39 @@ namespace Citizen
 {
     class Program
     {
-        static void Main(string[] args) => ListProjectConfigurations().Wait();
+        static void Main(string[] args) => ListBuilds().Wait();
 
-        private static async Task ListProjectConfigurations()
+        private static async Task ListBuilds()
         {
             var teamCityHost = Parameters.GetTeamCityHost();
             var authenticationHeader = Parameters.GetAuthenticationHeader();
             var client = new HttpClient();
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authenticationHeader);
-
-            var builds = await BuildSource.GetBuilds(client, teamCityHost);
+            var buildSource = new BuildSource(client, teamCityHost);
+            var builds = await buildSource.GetBuilds();
             
             foreach (var build in builds)
             {
                 Console.WriteLine($"Build {build.Id} started {build.Started}, status {build.Status}");
+            }
+
+            var statisticsFactory = new BuildStatisticsFactory();
+            var successStatistics = statisticsFactory.CreateStatistics(builds, "SUCCESS");
+            Console.WriteLine("SUCCESS");
+            PrintBuildStatistics(successStatistics);
+            var failureStatistics = statisticsFactory.CreateStatistics(builds, "FAILURE");
+            Console.WriteLine("FAILURE");
+            PrintBuildStatistics(failureStatistics);
+        }
+
+        private static void PrintBuildStatistics(IEnumerable<Tuple<string, int, double, double>> statistics)
+        {
+            foreach (var stat in statistics.OrderByDescending(s => s.Item2))
+            {
+                Console.Write($"#{stat.Item2:D3}");
+                Console.Write($" lag {TimeSpan.FromSeconds(stat.Item3):hh\\:mm\\:ss}");
+                Console.Write($" run {TimeSpan.FromSeconds(stat.Item4):hh\\:mm\\:ss}");
+                Console.WriteLine($" ({stat.Item1})");
             }
         }
     }
